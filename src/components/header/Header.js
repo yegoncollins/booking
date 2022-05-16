688b730c90bd5f6e5783741b51d4ee9d3a0a117d
yegoncollins <collinsyegon816@gmail.com>
2022-05-16 08:42:14 +0300

    first commite

---
+++ src/components/header/Header.js
@@ -0,0 +1,16 @@
+import "./header.css"
+
+function Header() {
+  return (
+    <div className="header">
+       <div className="headerList">
+           <div className="headerListItem">
+
+           </div>
+       </div>
+
+    </div>
+  )
+}
+
+export default Header
---
+++ src/pages/home/Home.js
@@ -0,0 +1,13 @@
+import Navbar from "../../components/Navbar";
+import Header from "../../components/header/Header";
+import "./home.css"
+
+export const Home = () => {
+  return (
+    <div>
+          <Navbar/>
+          <Header/>
+    </div>
+  );
+}
+export default Home;
